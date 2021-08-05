c38b757431e3505ec1a49173989333a542d8c677
rpagett <riley.pagett@gmail.com>
2021-08-05 18:47:10 -0400

    A trail of shame in git blame

--- config/environment.js
+++ config/environment.js
@@ -1,10 +1,10 @@
-'use strict';
+'use strict'
 
-module.exports = function (environment) {
+module.exports = function ( environment ) {
   let ENV = {
     modulePrefix: 'pokemonde',
-    environment: 'development', //environment
-    rootURL: '/',
+    environment,
+    rootURL: '/pokemonde/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -21,9 +21,9 @@ module.exports = function (environment) {
       // Here you can pass flags/options to your application instance
       // when it is created
     },
-  };
+  }
 
-  if (environment === 'development') {
+  if ( environment === 'development' ) {
     // ENV.APP.LOG_RESOLVER = true
     // ENV.APP.LOG_ACTIVE_GENERATION = true
     // ENV.APP.LOG_TRANSITIONS = true
@@ -31,21 +31,21 @@ module.exports = function (environment) {
     // ENV.APP.LOG_VIEW_LOOKUPS = true
   }
 
-  if (environment === 'test') {
+  if ( environment === 'test' ) {
     // Testem prefers this...
-    ENV.locationType = 'none';
+    ENV.locationType = 'none'
 
     // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+    ENV.APP.LOG_ACTIVE_GENERATION = false
+    ENV.APP.LOG_VIEW_LOOKUPS = false
 
-    ENV.APP.rootElement = '#ember-testing';
-    ENV.APP.autoboot = false;
+    ENV.APP.rootElement = '#ember-testing'
+    ENV.APP.autoboot = false
   }
 
-  if (environment === 'production') {
+  if ( environment === 'production' ) {
     // here you can enable a production-specific feature
   }
 
-  return ENV;
-};
+  return ENV
+}
