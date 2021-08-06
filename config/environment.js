8588cb50bfaafb617c1fa11ec1290acc8458fa05
rpagett <riley.pagett@gmail.com>
2021-08-06 10:25:18 -0400

    hash routing

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function ( environment ) {
     modulePrefix: 'pokemonde',
     environment,
     rootURL: '/pokemonde/',
-    locationType: 'auto',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
